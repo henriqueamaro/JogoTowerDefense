64350c081322bd148f27427b6038c378bf27c5a8
Felipe Henrique <felipehenriqueam@gmail.com>
2019-09-06 22:28:25 -0300

    projeto

---
+++ Tower Defense/Assets/Script/Inimigo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Inimigo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agente = GetComponent<NavMeshAgent>();
+        GameObject fimDoCaminho = GameObject.Find("FimDoCaminho");
+        Vector3 posicaoFimDoCaminho = fimDoCaminho.transform.position;
+        agente.SetDestination(posicaoFimDoCaminho);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
